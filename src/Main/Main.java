f4efd508c7f9ed242764b4f759d3d4528f31ec3d
JacentyI1 <jacek.kwiecinski@student.put.poznan.pl>
2023-04-08 15:37:05 +0200

    idk

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -11,11 +11,11 @@ public class Main {
     public static void main(String[] args){
         ArrayList<Integer> file_data = new ArrayList<>();
         FileOp.readFile(file_data); // reads specified file
-//        ArrayList<Integer> sBox = extractVal(file_data); // extracts actual values from s-box
-//        var hm = extractFun(sBox); // extracts each function from s-box values
+        ArrayList<Integer> sBox = extractVal(file_data); // extracts actual values from s-box
+        var hm = extractFun(sBox); // extracts each function from s-box values
         // extracts s-box functions from the values
-//        HashMap<Integer, ArrayList<Integer>> linFun = genLinearFun(getBasicFun());
-//        getBalance(linFun);
+        HashMap<Integer, ArrayList<Integer>> linFun = genLinearFun(getBasicFun());
+        getBalance(linFun);
 
 
     }
