994f238cf5d2d03c8e0b3e94193be0be1af86892
JacentyI1 <jacek.kwiecinski@student.put.poznan.pl>
2023-04-20 06:39:51 +0200

    i don't even know anymore

--- src/lib/ImageEnc.java
+++ src/lib/ImageEnc.java
@@ -1,11 +1,14 @@
 package lib;
 
+import org.w3c.dom.css.RGBColor;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ImageEnc {
     String path;
@@ -25,12 +28,21 @@ public class ImageEnc {
         }
         return image;
     }
+    public BufferedImage getImage(String p) {
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(new File(p));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
+    }
 
     public int input(int y, int x, int width){
         return (y*width)+x;
     }
 
-    public void encrypt(){
+    public void simpleEncrypt(){
         ArrayList<Integer> sbox = new ArrayList<>();
         FileOp file = new FileOp();
         file.readSboxFile(sbox, sBoxPath);
@@ -44,12 +56,12 @@ public class ImageEnc {
                 int r = color.getRed();
                 int g = color.getGreen();
                 int b = color.getBlue();
-
                 int encryptedR = sbox.get(r);
                 int encryptedG = sbox.get(g);
                 int encryptedB = sbox.get(b);
                 int encryptedPixel = (encryptedR<<16) | (encryptedG<<8) | encryptedB;
                 image.setRGB(x, y, encryptedPixel);
+                Collections.rotate(sbox, 1);
             }
         }
         try {
@@ -58,4 +70,30 @@ public class ImageEnc {
             e.printStackTrace();
         }
     }
+
+    public void simpleDecript(){
+        ArrayList<Integer> sbox = new ArrayList<>();
+        FileOp file = new FileOp();
+        file.readSboxFile(sbox, sBoxPath);
+
+        BufferedImage image = this.getImage("src/res/encrypted_image.jpg");
+        for(int y=0; y<image.getHeight(); y++){
+            for(int x=0; x<image.getWidth(); x++){
+                int pixel = image.getRGB(x, y);
+                Color color = new Color(pixel);
+                int r = sbox.indexOf(color.getRed());
+                int g = sbox.indexOf(color.getGreen());
+                int b = sbox.indexOf(color.getBlue());
+                int decriptPixel = (r << 16) | (g << 8) | b;
+                image.setRGB(x, y, decriptPixel);
+                Collections.rotate(sbox,1);
+            }
+        }
+        try {
+            ImageIO.write(image, "jpg", new File("src/res/decrypted_image.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/main.java
+++ src/main.java
@@ -1,9 +1,15 @@
 import lib.ImageEnc;
 
+import static lib.actions.testBox;
+
 public class main {
     public static void main(String[] args){
-        ImageEnc test = new ImageEnc("src/res/mountins.jpg");
-        test.encrypt();
-//        testBox();
+//        ImageEnc test = new ImageEnc("src/res/blue.jpg");
+//        test.simpleEncrypt(); // 2-3 sec
+//        test.simpleDecript(); // 2-3 sec
+//        test.simpleDecript();
+//        test.simpleEncrypt("src/res/encrypted_image.jpg");
+//        test.simpleDecript();
+        testBox();
     }
 }
